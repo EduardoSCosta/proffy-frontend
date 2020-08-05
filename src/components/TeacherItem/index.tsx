e73b25f27068f28649121ac21086b64bed6ebc20
EduardoSCosta <edu_costaaraujo@hotmail.com>
2020-08-05 20:35:44 -0300

    Teacher list page

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4Bwz8-TWJNKPdLhikrDm97LAOm7OJQgCIgQ" alt="Professor Exemplo"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Professor Exemplo</strong>
-        <span>Química</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      <br/><br/>
-      Nihil, illo neque? Labore voluptatem numquam repellat deleniti mollitia aspernatur debitis fugit autem voluptatum, molestias, nisi nostrum aperiam nihil facere unde maxime.
-    </p>
+    <p>{teacher.bio}</p>
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 50,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsappIcon} alt="Whatsapp"/>
         Entrar em Contato
-      </button>
+      </a>
     </footer>
   </article>
   );
